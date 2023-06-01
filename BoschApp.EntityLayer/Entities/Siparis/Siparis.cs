dc86b94a710c7ef4546bbfcd9f32074c81a84d8f
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:07:36 +0300

    Siparis and Uretim entities are added

---
+++ BoschApp.EntityLayer/Entities/Siparis/Siparis.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.Siparis
+{
+    public class Siparis
+    {
+        public int Id { get; set; }
+        public int Adet { get; set; }
+        public int SiparisDurumu { get; set; }
+    }
+}
