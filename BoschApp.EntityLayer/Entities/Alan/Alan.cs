07d524536c557ea0bc11547481209a4da92c3677
Bahadır <oxikairos@hotmail.com>
2023-06-01 23:50:11 +0300

    Alan and Departman entities are added

---
+++ BoschApp.EntityLayer/Entities/Alan/Alan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.Alan
+{
+    public class Alan
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+    }
+}
---
+++ BoschApp.EntityLayer/Entities/Departman/Departman.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.Departman
+{
+    public class Departman
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public float Butce { get; set; }
+    }
+}
