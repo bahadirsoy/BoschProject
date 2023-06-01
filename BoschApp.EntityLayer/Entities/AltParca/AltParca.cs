76f7fc6fdbdfe886ab74ed5e446764679b0e5a6c
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:09:56 +0300

    AltParca and Istasyon entities are added

---
+++ BoschApp.EntityLayer/Entities/AltParca/AltParca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.AltParca
+{
+    public class AltParca
+    {
+        public int Id { get; set; }
+        public float Masraf { get; set; }
+    }
+}
