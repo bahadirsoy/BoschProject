7c129130bf0f001686627675d534d691c14775de
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:52:36 +0300

    Departman and Kisim one-to-many relation is added

--- BoschApp.EntityLayer/Entities/DepartmanEntity/Departman.cs
+++ BoschApp.EntityLayer/Entities/DepartmanEntity/Departman.cs
@@ -1,4 +1,5 @@
 ﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace BoschApp.EntityLayer.Entities.DepartmanEntityEntity
         public string Isim { get; set; }
         public float Butce { get; set; }
         public Alan Alan { get; set; }
+        public ICollection<Kisim> Kisims { get; set; }
     }
 }
--- BoschApp.EntityLayer/Entities/KisimEntity/Kisim.cs
+++ BoschApp.EntityLayer/Entities/KisimEntity/Kisim.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,5 +11,6 @@ namespace BoschApp.EntityLayer.Entities.KisimEntity
     {
         public int Id { get; set; }
         public string KisimNo { get; set; }
+        public Departman Departman { get; set; }
     }
 }
