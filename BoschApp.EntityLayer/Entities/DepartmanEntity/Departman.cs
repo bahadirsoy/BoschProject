f03b2db388248e7c624d48a8e6f4a83eda6146f2
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:41:47 +0300

    Alan and departman one-to-many relation is added

--- BoschApp.EntityLayer/Entities/DepartmanEntity/Departman.cs
+++ BoschApp.EntityLayer/Entities/DepartmanEntity/Departman.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,6 @@ namespace BoschApp.EntityLayer.Entities.DepartmanEntityEntity
         public int Id { get; set; }
         public string Isim { get; set; }
         public float Butce { get; set; }
+        public Alan Alan { get; set; }
     }
 }
