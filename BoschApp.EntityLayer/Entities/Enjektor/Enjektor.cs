9ee818521b21ae8466ac474a9546340860054010
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:04:45 +0300

    Enjektor and Kisim entities are added

---
+++ BoschApp.EntityLayer/Entities/Enjektor/Enjektor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.Enjektor
+{
+    public class Enjektor
+    {
+        public int Id { get; set; }
+        public string Tur { get; set; }
+        public string ModelNo { get; set; }
+    }
+}
