87554632e1cce5349a38b97d2ca0ab20f28d67ab
Bahadır <oxikairos@hotmail.com>
2023-06-02 00:10:38 +0300

    StokAlani entity is added

---
+++ BoschApp.EntityLayer/Entities/StokAlani/StokAlani.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.StokAlani
+{
+    public class StokAlani
+    {
+        public int Id { get; set; }
+        public int StokAdeti { get; set; }
+    }
+}
