c554d545fcf5bfb4d2cdf97e132c38f9b3ab68dc
Bahadır <oxikairos@hotmail.com>
2023-06-02 18:38:23 +0300

    Alan repository interface is added

---
+++ BoschApp.DataAccessLayer/Abstract/IAlanRepository.cs
@@ -0,0 +1,14 @@
+﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IAlanRepository
+    {
+        ICollection<Alan> GetAlans();
+    }
+}
