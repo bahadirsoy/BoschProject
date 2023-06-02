ed695fec7236f7d01325bc7232d4daa241e1233b
Bahadır <oxikairos@hotmail.com>
2023-06-02 11:13:47 +0300

    EnjektorAltParca entity is added

---
+++ BoschApp.EntityLayer/Entities/EnjektorAltParcaEntity/EnjektorAltParca.cs
@@ -0,0 +1,18 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.EntityLayer.Entities.EnjektorAltParcaEntity
+{
+    public class EnjektorAltParca
+    {
+        public int EnjektorId { get; set; }
+        public int AltParcaId { get; set; }
+        public Enjektor Enjektor { get; set; }
+        public AltParca AltParca { get; set; }
+    }
+}
