147ffc54d741cea8e7e83641d28ca7e2354cf16c
Bahadır <oxikairos@hotmail.com>
2023-06-02 19:12:50 +0300

    Alan business service interface is added

---
+++ BoschApp.BusinessLayer/Abstract/IAlanBusinessService.cs
@@ -0,0 +1,14 @@
+﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Abstract
+{
+    public interface IAlanBusinessService
+    {
+        ICollection<Alan> GetAlans();
+    }
+}
