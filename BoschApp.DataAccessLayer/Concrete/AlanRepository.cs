a83c8cb154dae0cc58bd0cb13e773c5b2e9b86c6
Bahadır <oxikairos@hotmail.com>
2023-06-02 18:42:27 +0300

    Alan repository is added

---
+++ BoschApp.DataAccessLayer/Concrete/AlanRepository.cs
@@ -0,0 +1,26 @@
+﻿using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.DataAccessLayer.Data;
+using BoschApp.EntityLayer.Entities.AlanEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Concrete
+{
+    public class AlanRepository : IAlanRepository
+    {
+        private readonly DataContext _context;
+
+        public AlanRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Alan> GetAlans()
+        {
+            return _context.Alans.ToList();
+        }
+    }
+}
