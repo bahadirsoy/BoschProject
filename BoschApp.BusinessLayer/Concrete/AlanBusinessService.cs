61b4a41a26b4fad32de53946c7190ff493751faa
Bahadır <oxikairos@hotmail.com>
2023-06-02 19:25:44 +0300

    Alan business service is added

---
+++ BoschApp.BusinessLayer/Concrete/AlanBusinessService.cs
@@ -0,0 +1,26 @@
+﻿using BoschApp.BusinessLayer.Abstract;
+using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.EntityLayer.Entities.AlanEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Concrete
+{
+    public class AlanBusinessService : IAlanBusinessService
+    {
+        private readonly IAlanRepository _alanRepository;
+
+        public AlanBusinessService(IAlanRepository alanRepository)
+        {
+            _alanRepository = alanRepository;
+        }
+
+        public ICollection<Alan> GetAlans()
+        {
+            return _alanRepository.GetAlans();
+        }
+    }
+}
