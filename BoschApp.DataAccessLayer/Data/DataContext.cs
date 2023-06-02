9bbba68a4c5e1e50e0c94aaf521bd819ed490554
Bahadır <oxikairos@hotmail.com>
2023-06-02 12:42:51 +0300

    all tables are added to the DataContext

--- BoschApp.DataAccessLayer/Data/DataContext.cs
+++ BoschApp.DataAccessLayer/Data/DataContext.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using BoschApp.EntityLayer.Entities.EnjektorAltParcaEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.IstasyonEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using BoschApp.EntityLayer.Entities.UretimEntity;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +23,16 @@ namespace BoschApp.DataAccessLayer.Data
         {
             
         }
+
+        public DbSet<Alan> Alans { get; set; }
+        public DbSet<AltParca> AltParcas { get; set; }
+        public DbSet<Departman> Departmants { get; set; }
+        public DbSet<EnjektorAltParca> EnjektorAltParcas { get; set; }
+        public DbSet<Enjektor> Enjektors { get; set; }
+        public DbSet<Istasyon> Istasyons { get; set; }
+        public DbSet<Kisim> Kisims { get; set; }
+        public DbSet<Siparis> Siparises { get; set; }
+        public DbSet<StokAlani> StokAlanis { get; set; }
+        public DbSet<Uretim> Uretims { get; set; }
     }
 }
