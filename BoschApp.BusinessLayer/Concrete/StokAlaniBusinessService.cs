e25f7aa2c8fe2bad82618d3c2a7e74a7baf3d077
Bahadır <oxikairos@hotmail.com>
2023-06-03 19:07:35 +0300

    StokAlaniBusinessService is created

---
+++ BoschApp.BusinessLayer/Concrete/StokAlaniBusinessService.cs
@@ -0,0 +1,33 @@
+﻿using BoschApp.BusinessLayer.Abstract;
+using BoschApp.BusinessLayer.Rules;
+using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Concrete
+{
+    public class StokAlaniBusinessService : IStokAlaniBusinessService
+    {
+        private readonly IStokAlaniBusinessService _stokAlaniBusinessService;
+        private readonly BusinessRules _rules;
+
+        public StokAlaniBusinessService(IStokAlaniBusinessService stokAlaniBusinessService, BusinessRules rules)
+        {
+            _stokAlaniBusinessService = stokAlaniBusinessService;
+            _rules = rules;
+        }
+
+        public StokAlani GetStokAlani(int stokAlaniId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<StokAlani> GetStokAlanis()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
