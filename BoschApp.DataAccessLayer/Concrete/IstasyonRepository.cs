4fae39fd512a0a647ae96eef2b76e6ff637bb155
Bahadır <oxikairos@hotmail.com>
2023-06-03 17:32:18 +0300

    IstasyonRepository is added

---
+++ BoschApp.DataAccessLayer/Concrete/IstasyonRepository.cs
@@ -0,0 +1,42 @@
+﻿using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.DataAccessLayer.Data;
+using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.IstasyonEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Concrete
+{
+    public class IstasyonRepository : IIstasyonRepository
+    {
+        private readonly DataContext _context;
+
+        public IstasyonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<AltParca> GetAltParcasByIstasyon(int istasyonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Istasyon GetIstasyon(int istasyonId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Istasyon> GetIstasyons()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IstasyonExist(int istasyonId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
