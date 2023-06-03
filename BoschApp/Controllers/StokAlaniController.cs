c01a4218ce15d67891b06603a6848c9a504ca8e7
Bahadır <oxikairos@hotmail.com>
2023-06-03 19:12:43 +0300

    StokAlaniController is created

---
+++ BoschApp/Controllers/StokAlaniController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StokAlaniController : Controller
+    {
+        private readonly IStokAlaniBusinessService _stokAlaniBusinessService;
+        private readonly IMapper _mapper;
+
+        public StokAlaniController(IStokAlaniBusinessService stokAlaniBusinessService, IMapper mapper)
+        {
+            _stokAlaniBusinessService = stokAlaniBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
