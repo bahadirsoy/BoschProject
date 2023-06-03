5272c916db021759af007e9e64678743d8556e48
Bahadır <oxikairos@hotmail.com>
2023-06-03 12:35:18 +0300

    IDepartmanRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/IDepartmanRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoschApp.EntityLayer.Entities.AlanEntity;
+using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IDepartmanRepository
+    {
+        ICollection<Departman> GetDepartmans();
+        bool DepartmanExist(int departmanId);
+        Departman GetDepartman(int departmanId);
+        Alan GetAlanByDepartman(int departmanId);
+        ICollection<Kisim> GetKisimsByDepartman(int departmanId);
+    }
+}
