52b9f22d333a7909530c56dfb422afcfbb99dafd
Bahadır <oxikairos@hotmail.com>
2023-06-03 15:09:46 +0300

    IKisimBusinessService is added

---
+++ BoschApp.BusinessLayer/Abstract/IKisimBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Abstract
+{
+    public interface IKisimBusinessService
+    {
+        ICollection<Kisim> GetKisims();
+        Kisim GetKisim(int kisimId);
+        Departman GetDepartmanByKisim(int kisimId);
+        ICollection<Enjektor> GetEnjektorsByKisim(int kisimId);
+    }
+}
