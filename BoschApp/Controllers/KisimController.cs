f799e58056f9cd9122f963d274d33356161b5dce
Bahadır <oxikairos@hotmail.com>
2023-06-03 15:32:50 +0300

    KisimController is created

---
+++ BoschApp/Controllers/KisimController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KisimController : Controller
+    {
+        private readonly IKisimBusinessService _kisimBusinessService;
+        private readonly IMapper _mapper;
+
+        public KisimController(IKisimBusinessService kisimBusinessService, IMapper mapper)
+        {
+            _kisimBusinessService = kisimBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
