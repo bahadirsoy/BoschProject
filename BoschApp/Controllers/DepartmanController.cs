1c3ace0a126eb8c54e2f196f5dffa2d257eb759a
Bahadır <oxikairos@hotmail.com>
2023-06-03 13:03:13 +0300

    DepartmanController is created

---
+++ BoschApp/Controllers/DepartmanController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmanController
+    {
+        private readonly IDepartmanBusinessService _departmanBusinessService;
+        private readonly IMapper _mapper;
+
+        public DepartmanController(IDepartmanBusinessService departmanBusinessService, IMapper mapper)
+        {
+            _departmanBusinessService = departmanBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
