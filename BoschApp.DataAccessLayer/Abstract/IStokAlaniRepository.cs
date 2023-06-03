abbabc1a98e197917bfc84352c065a7953e933a1
Bahadır <oxikairos@hotmail.com>
2023-06-03 18:58:42 +0300

    IStokAlaniRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/IStokAlaniRepository.cs
@@ -0,0 +1,16 @@
+﻿using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IStokAlaniRepository
+    {
+        ICollection<StokAlani> GetStokAlanis();
+        bool StokAlaniExist(int stokAlaniId);
+        StokAlani GetStokAlani(int stokAlaniId);
+    }
+}
