2a6186fa5962c9c0e686b55073662090af85e16c
Bahadır <oxikairos@hotmail.com>
2023-06-03 15:11:52 +0300

    KisimBusinessService is added

---
+++ BoschApp.BusinessLayer/Concrete/KisimBusinessService.cs
@@ -0,0 +1,46 @@
+﻿using BoschApp.BusinessLayer.Abstract;
+using BoschApp.BusinessLayer.Rules;
+using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Concrete
+{
+    public class KisimBusinessService : IKisimBusinessService
+    {
+        private readonly IKisimRepository _kisimRepository;
+        private readonly BusinessRules _rules;
+
+        public KisimBusinessService(IKisimRepository kisimRepository, BusinessRules rules)
+        {
+            _kisimRepository = kisimRepository;
+            _rules = rules;
+        }
+
+        public Departman GetDepartmanByKisim(int kisimId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Enjektor> GetEnjektorsByKisim(int kisimId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Kisim GetKisim(int kisimId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Kisim> GetKisims()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
