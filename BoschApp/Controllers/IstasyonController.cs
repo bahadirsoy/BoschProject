45d12e772773c3682c39ac5341a1a6b5c2223fb3
Bahadır <oxikairos@hotmail.com>
2023-06-03 17:58:18 +0300

    IstasyonController is created

---
+++ BoschApp/Controllers/IstasyonController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IstasyonController : Controller
+    {
+        private readonly IIstasyonBusinessService _istasyonBusinessService;
+        private readonly IMapper _mapper;
+
+        public IstasyonController(IIstasyonBusinessService istasyonBusinessService, IMapper mapper)
+        {
+            _istasyonBusinessService = istasyonBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
