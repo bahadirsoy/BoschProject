dd48ab9ccc514b9b223e6b1de313e8ea4dd21d15
Bahadır <oxikairos@hotmail.com>
2023-06-03 14:57:55 +0300

    IKisimRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/IKisimRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IKisimRepository
+    {
+        ICollection<Kisim> GetKisims();
+        bool KisimExist(int kisimId);
+        Kisim GetKisim(int kisimId);
+        Departman GetDepartmanByKisim(int kisimId);
+        ICollection<Enjektor> GetEnjektorsByKisim(int kisimId);
+    }
+}
