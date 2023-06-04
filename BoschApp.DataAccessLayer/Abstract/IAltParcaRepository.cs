a8c0e54bfe9d03347c1a93c560b2c45f8429eedf
Bahadır <oxikairos@hotmail.com>
2023-06-04 10:56:47 +0300

    IAltParcaRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/IAltParcaRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.IstasyonEntity;
+using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IAltParcaRepository
+    {
+        ICollection<AltParca> GetAltParcas();
+        bool AltParcaExist(int altParcaId);
+        AltParca GetAltParca(int altParcaId);
+        StokAlani GetStokAlaniByAltParca(int altParcaId);
+        Istasyon GetIstasyonByAltParca(int altParcaId);
+    }
+}
