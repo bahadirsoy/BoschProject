9a9787bb397dfdb75d1e69f5c325b47c2e8086ca
Bahadır <oxikairos@hotmail.com>
2023-06-04 15:10:46 +0300

    SiparisBusinessService is created

---
+++ BoschApp.BusinessLayer/Concrete/SiparisBusinessService.cs
@@ -0,0 +1,46 @@
+﻿using BoschApp.BusinessLayer.Abstract;
+using BoschApp.BusinessLayer.Rules;
+using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using BoschApp.EntityLayer.Entities.UretimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Concrete
+{
+    public class SiparisBusinessService : ISiparisBusinessService
+    {
+        private readonly ISiparisRepository _siparisRepository;
+        private readonly BusinessRules _rules;
+
+        public SiparisBusinessService(ISiparisRepository siparisRepository, BusinessRules rules)
+        {
+            _siparisRepository = siparisRepository;
+            _rules = rules;
+        }
+
+        public Enjektor GetEnjektorBySiparis(int siparisId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Siparis GetSiparis(int siparisId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Siparis> GetSiparises()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Uretim> GetUretimsBySiparis(int siparisId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
