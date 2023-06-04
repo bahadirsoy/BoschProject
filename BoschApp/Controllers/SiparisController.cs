ba92f5d1d7a51f88d31c65697a3636a6ea50d070
Bahadır <oxikairos@hotmail.com>
2023-06-04 15:21:17 +0300

    SiparisController is created

---
+++ BoschApp/Controllers/SiparisController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SiparisController : Controller
+    {
+        private readonly ISiparisBusinessService _siparisBusinessService;
+        private readonly IMapper _mapper;
+
+        public SiparisController(ISiparisBusinessService siparisBusinessService, IMapper mapper)
+        {
+            _siparisBusinessService = siparisBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
