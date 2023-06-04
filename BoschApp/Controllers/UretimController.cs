994681270e8adb81c163a9030fe911ddb356dafd
Bahadır <oxikairos@hotmail.com>
2023-06-04 17:09:05 +0300

    UretimController is created

---
+++ BoschApp/Controllers/UretimController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UretimController : Controller
+    {
+        private readonly IUretimBusinessService _uretimBusinessService;
+        private readonly IMapper _mapper;
+
+        public UretimController(IUretimBusinessService uretimBusinessService, IMapper mapper)
+        {
+            _uretimBusinessService = uretimBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
