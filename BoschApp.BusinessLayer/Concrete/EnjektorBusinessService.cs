fb145aef223f236bec365dc485ca97e94ab59ec3
Bahadır <oxikairos@hotmail.com>
2023-06-04 14:23:30 +0300

    EnjektorBusinessService methods are implemented

--- BoschApp.BusinessLayer/Concrete/EnjektorBusinessService.cs
+++ BoschApp.BusinessLayer/Concrete/EnjektorBusinessService.cs
@@ -2,6 +2,7 @@
 using BoschApp.BusinessLayer.Rules;
 using BoschApp.DataAccessLayer.Abstract;
 using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.DepartmanEntityEntity;
 using BoschApp.EntityLayer.Entities.EnjektorEntity;
 using BoschApp.EntityLayer.Entities.KisimEntity;
 using System;
@@ -25,22 +26,65 @@ namespace BoschApp.BusinessLayer.Concrete
 
         public ICollection<AltParca> GetAltParcasByEnjektor(int enjektorId)
         {
-            throw new NotImplementedException();
+            if (!_enjektorRepository.EnjektorExist(enjektorId))
+            {
+                throw new Exception("There is no enjektor with id: " + enjektorId);
+            }
+
+            var altParcas = _enjektorRepository.GetAltParcasByEnjektor(enjektorId);
+
+            if (_rules.IsNull(altParcas))
+            {
+                throw new Exception("This enjektor has no altParca");
+            }
+
+            return altParcas;
         }
 
         public Enjektor GetEnjektor(int enjektorId)
         {
-            throw new NotImplementedException();
+            if (!_enjektorRepository.EnjektorExist(enjektorId))
+            {
+                throw new Exception("There is no enjektor with id: " + enjektorId);
+            }
+
+            var enjektor = _enjektorRepository.GetEnjektor(enjektorId);
+
+            if (_rules.IsNull(enjektor))
+            {
+                throw new Exception("There is no such enjektor");
+            }
+
+            return enjektor;
         }
 
         public ICollection<Enjektor> GetEnjektors()
         {
-            throw new NotImplementedException();
+            var enjektors = _enjektorRepository.GetEnjektors();
+
+            if (_rules.IsNull(enjektors))
+            {
+                throw new Exception("There is no enjektor");
+            }
+
+            return enjektors;
         }
 
         public Kisim GetKisimByEnjektor(int enjektorId)
         {
-            throw new NotImplementedException();
+            if (!_enjektorRepository.EnjektorExist(enjektorId))
+            {
+                throw new Exception("This enjektor has no Kisim");
+            }
+
+            var kisim = _enjektorRepository.GetKisimByEnjektor(enjektorId);
+
+            if (_rules.IsNull(kisim))
+            {
+                throw new Exception("There is no Kisim");
+            }
+
+            return kisim;
         }
     }
 }
