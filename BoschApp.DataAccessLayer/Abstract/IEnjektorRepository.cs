540595ba7925a331373d2c4711b5152f7e8e60af
Bahadır <oxikairos@hotmail.com>
2023-06-04 14:04:10 +0300

    IEnjektorRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/IEnjektorRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface IEnjektorRepository
+    {
+        ICollection<Enjektor> GetEnjektors();
+        bool EnjektorExist(int enjektorId);
+        Enjektor GetEnjektor(int enjektorId);
+        Kisim GetKisimByEnjektor(int enjektorId);
+        ICollection<AltParca> GetAltParcasByEnjektor(int enjektorId);
+    }
+}
