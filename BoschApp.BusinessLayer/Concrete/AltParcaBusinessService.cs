f6f1725ba42517d7c2e472f0066fba2cdb5c881c
Bahadır <oxikairos@hotmail.com>
2023-06-04 11:08:56 +0300

    AltParcaBusinessService is created

--- BoschApp.BusinessLayer/Concrete/AltParcaBusinessService.cs
+++ BoschApp.BusinessLayer/Concrete/AltParcaBusinessService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using BoschApp.BusinessLayer.Abstract;
+using BoschApp.BusinessLayer.Rules;
+using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.IstasyonEntity;
+using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,35 @@ using System.Threading.Tasks;
 
 namespace BoschApp.BusinessLayer.Concrete
 {
-    internal class AltParcaBusinessService
+    public class AltParcaBusinessService : IAltParcaBusinessService
     {
+        private readonly IAltParcaRepository _altParcaRepository;
+        private readonly BusinessRules _rules;
+
+        public AltParcaBusinessService(IAltParcaRepository altParcaRepository, BusinessRules rules)
+        {
+            _altParcaRepository = altParcaRepository;
+            _rules = rules;
+        }
+
+        public AltParca GetAltParca(int altParcaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<AltParca> GetAltParcas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Istasyon GetIstasyonByAltParca(int altParcaId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StokAlani GetStokAlaniByAltParca(int altParcaId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
