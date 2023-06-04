f2150e98e18071452f83a44e37d9716b4125720f
Bahadır <oxikairos@hotmail.com>
2023-06-04 16:58:13 +0300

    UretimRepository is created

---
+++ BoschApp.DataAccessLayer/Concrete/UretimRepository.cs
@@ -0,0 +1,37 @@
+﻿using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.DataAccessLayer.Data;
+using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Concrete
+{
+    public class UretimRepository : IUretimRepository
+    {
+        private readonly DataContext _context;
+
+        public UretimRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public AltParca GetAltParcaByUretim(int uretimId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Siparis GetSiparisByUretim(int uretimId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UretimExist(int uretimId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
