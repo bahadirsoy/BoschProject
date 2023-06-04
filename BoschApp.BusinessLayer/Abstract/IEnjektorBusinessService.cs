696866c247ef3842a1afcfa86221ee1364f4f968
Bahadır <oxikairos@hotmail.com>
2023-06-04 14:14:23 +0300

    IEnjektorBusinessService is added

---
+++ BoschApp.BusinessLayer/Abstract/IEnjektorBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.KisimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Abstract
+{
+    public interface IEnjektorBusinessService
+    {
+        ICollection<Enjektor> GetEnjektors();
+        Enjektor GetEnjektor(int enjektorId);
+        Kisim GetKisimByEnjektor(int enjektorId);
+        ICollection<AltParca> GetAltParcasByEnjektor(int enjektorId);
+    }
+}
