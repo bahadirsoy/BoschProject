def733c8fb30457111e4187ee3949a8c9558943d
Bahadır <oxikairos@hotmail.com>
2023-06-04 14:29:59 +0300

    EnjektorController is created

---
+++ BoschApp/Controllers/EnjektorController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BoschApp.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoschApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnjektorController : Controller
+    {
+        private readonly IEnjektorBusinessService _enjektorBusinessService;
+        private readonly IMapper _mapper;
+
+        public EnjektorController(IEnjektorBusinessService enjektorBusinessService, IMapper mapper)
+        {
+            _enjektorBusinessService = enjektorBusinessService;
+            _mapper = mapper;
+        }
+    }
+}
