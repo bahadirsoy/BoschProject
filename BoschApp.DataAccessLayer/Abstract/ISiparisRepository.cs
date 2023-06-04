ad0e235d3f0fc1a11a80ad6159694d3cc21b93f7
Bahadır <oxikairos@hotmail.com>
2023-06-04 14:57:53 +0300

    ISiparisRepository is added

---
+++ BoschApp.DataAccessLayer/Abstract/ISiparisRepository.cs
@@ -0,0 +1,20 @@
+﻿using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using BoschApp.EntityLayer.Entities.UretimEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.DataAccessLayer.Abstract
+{
+    public interface ISiparisRepository
+    {
+        ICollection<Siparis> GetSiparises();
+        bool SiparisExist(int siparisId);
+        Siparis GetSiparis(int siparisId);
+        Enjektor GetEnjektorBySiparis(int siparisId);
+        ICollection<Uretim> GetUretimsBySiparis(int siparisId);
+    }
+}
