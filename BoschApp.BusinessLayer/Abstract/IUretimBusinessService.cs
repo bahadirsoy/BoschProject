6c1ad61e5f958a28c3931a52b95a27a3a985f9a8
Bahadır <oxikairos@hotmail.com>
2023-06-04 17:01:42 +0300

    IUretimBusinessService is added

---
+++ BoschApp.BusinessLayer/Abstract/IUretimBusinessService.cs
@@ -0,0 +1,16 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Abstract
+{
+    public interface IUretimBusinessService
+    {
+        Siparis GetSiparisByUretim(int uretimId);
+        AltParca GetAltParcaByUretim(int uretimId);
+    }
+}
