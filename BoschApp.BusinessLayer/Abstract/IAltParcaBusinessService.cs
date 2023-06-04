7eb644e973ce0c861a2357c0df4db218b5c5ccb7
Bahadır <oxikairos@hotmail.com>
2023-06-04 11:07:24 +0300

    IAltParcaBusinessService is added

---
+++ BoschApp.BusinessLayer/Abstract/IAltParcaBusinessService.cs
@@ -0,0 +1,19 @@
+﻿using BoschApp.EntityLayer.Entities.AltParcaEntity;
+using BoschApp.EntityLayer.Entities.IstasyonEntity;
+using BoschApp.EntityLayer.Entities.StokAlaniEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoschApp.BusinessLayer.Abstract
+{
+    public interface IAltParcaBusinessService
+    {
+        ICollection<AltParca> GetAltParcas();
+        AltParca GetAltParca(int altParcaId);
+        StokAlani GetStokAlaniByAltParca(int altParcaId);
+        Istasyon GetIstasyonByAltParca(int altParcaId);
+    }
+}
