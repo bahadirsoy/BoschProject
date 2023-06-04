bcc7e0ba4fff9bfdd5fae55f40ed29a830accca5
Bahadır <oxikairos@hotmail.com>
2023-06-04 15:05:23 +0300

    SiparisRepository methods are implemented

--- BoschApp.DataAccessLayer/Concrete/SiparisRepository.cs
+++ BoschApp.DataAccessLayer/Concrete/SiparisRepository.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BoschApp.DataAccessLayer.Abstract;
+using BoschApp.DataAccessLayer.Data;
+using BoschApp.EntityLayer.Entities.EnjektorEntity;
+using BoschApp.EntityLayer.Entities.SiparisEntity;
+using BoschApp.EntityLayer.Entities.UretimEntity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,38 @@ using System.Threading.Tasks;
 
 namespace BoschApp.DataAccessLayer.Concrete
 {
-    internal class SiparisRepository
+    public class SiparisRepository : ISiparisRepository
     {
+        private readonly DataContext _context;
+
+        public SiparisRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Enjektor GetEnjektorBySiparis(int siparisId)
+        {
+            return _context.Siparises.Where(s => s.Id == siparisId).Select(s => s.Enjektor).FirstOrDefault();
+        }
+
+        public Siparis GetSiparis(int siparisId)
+        {
+            return _context.Siparises.Where(s => s.Id == siparisId).FirstOrDefault();
+        }
+
+        public ICollection<Siparis> GetSiparises()
+        {
+            return _context.Siparises.ToList();
+        }
+
+        public ICollection<Uretim> GetUretimsBySiparis(int siparisId)
+        {
+            return _context.Uretims.Where(u => u.Siparis.Id == siparisId).ToList();
+        }
+
+        public bool SiparisExist(int siparisId)
+        {
+            return _context.Siparises.Any(s => s.Id == siparisId);
+        }
     }
 }
