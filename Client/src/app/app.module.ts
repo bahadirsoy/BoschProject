46cc36afe92ffb405e7edc989b7226de96ba7afe
Bahadır <oxikairos@hotmail.com>
2023-06-10 15:42:00 +0300

    HttpClientModule is added

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
@@ -10,7 +11,8 @@ import { HomepageComponent } from './components/homepage/homepage.component';
     HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
