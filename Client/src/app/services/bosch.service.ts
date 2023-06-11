0cfe30433e36c5be2e806c11b22119220619fa31
Bahadır <oxikairos@hotmail.com>
2023-06-11 12:09:51 +0300

    getAltParcaStoks service is added to boschService

--- Client/src/app/services/bosch.service.ts
+++ Client/src/app/services/bosch.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BoschService {
+  private apiUrl = 'https://localhost:7232/api/'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAltParcaStoks(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + "AltParca");
+  }
 }
