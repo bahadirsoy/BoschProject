b94a1d0e8c2bdabebe18ddc16bfa76440d2f70eb
Bahadır <oxikairos@hotmail.com>
2023-06-13 21:29:03 +0300

    istasyon variable in production is binded to istasyon component

--- Client/src/app/components/istasyon/istasyon.component.ts
+++ Client/src/app/components/istasyon/istasyon.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Istasyon } from 'src/app/models/Istasyon';
 
 @Component({
   selector: 'app-istasyon',
   templateUrl: './istasyon.component.html',
   styleUrls: ['./istasyon.component.css']
 })
-export class IstasyonComponent {
 
+export class IstasyonComponent {
+  @Input() istasyon!: Istasyon;
+
+  ngOnInit(): void {
+    
+  }
 }
