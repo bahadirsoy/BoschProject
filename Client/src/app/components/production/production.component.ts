f14235ac61e0537ca8ffc400c5a3ccff4de0ae0b
Bahadır <oxikairos@hotmail.com>
2023-06-13 12:11:12 +0300

    id parameter from route is fetched in production component

--- Client/src/app/components/production/production.component.ts
+++ Client/src/app/components/production/production.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-production',
   templateUrl: './production.component.html',
   styleUrls: ['./production.component.css']
 })
-export class ProductionComponent {
 
+export class ProductionComponent {
+  id!: string;
+
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id') as string;
+  }
 }
